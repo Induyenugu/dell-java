37891b23f6f86bdb76badf39d14cfd60a6023c3f
Yenugu <Indumathi_Yenugu@Dell.com>
2019-02-22 13:33:23 -0600

    fixed student grades as per comments

--- Grades/src/StudentGrades.java
+++ Grades/src/StudentGrades.java
@@ -3,69 +3,87 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class StudentGrades {
 
+	private static Scanner reader = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		
 		//Welcome note to user about Student grades program
 		System.out.println("Welcome to Student grades program");
 		System.out.println("It provides list of 10 students and their 5 grades and also gives average student grade for each student");
 		
+		//Take input from User
+		System.out.println("PLease enter no of students in your class");
+		
+		int input = 	CheckValidInput(reader.nextLine());	
+		
 		//Generates 10 students with 5 random grades
-		Map<String, int[]> studentandGrades= new HashMap<>();		
+		Map<String, String[]> studentandGrades= new HashMap<>();		
 		  
-        for (int i = 0; i < 10; i++) 
+        for (int i = 0; i < input; i++) 
         {
-        	String studentid= "studentid-"+String.valueOf(i);
-            int[] grades=generateRandomGrades();
-            studentandGrades.put(studentid, grades); 
-        	System.out.println("Added Student: "+ studentid +" and grades " + Arrays.toString(grades));
+        	System.out.println("Enter Student " + (i+1) +" Name");        	
+        	String studentid= reader.nextLine();
+        	System.out.println("Enter Student " + studentid+ " grades");   
+        	String[] studentgrades= reader.nextLine().split(",");
+        	studentandGrades.put(studentid, studentgrades);
         }   
+        reader.close();
         
         //Calculates and prints average grade of each student
         StudentsAvgGrade(studentandGrades);
         
-
+    	
 	}
 	
-	private static int[] generateRandomGrades()
-	{
-		int[] grades= new int[5]; 
-		Random rand = new Random();
-		 for (int i = 0; i < 5; i++)
-		 {
-			 int grade=rand.nextInt(100); 
-			 grades[i]=grade;
-		 }
-		 return grades;
-	        
+	//Verifies for valid inout from user
+	
+	private static int CheckValidInput(String input) {
+		int validinput=0;
+		try
+		{
+			validinput= Integer.parseInt(input);
+			if(validinput<=0)
+			{
+				System.out.println("PLease enter a valid number");			
+				CheckValidInput(reader.nextLine());	
+			}
+		}
+		catch(Exception ex)
+		{
+			System.out.println("PLease enter a valid number");			
+			CheckValidInput(reader.nextLine());	
+		}
+		return validinput;
 	}
 	
 	/*
 	 * Calculates and returns average grade for a given set of grades
 	 */
 	
-	private static double averageGrade(int[] grades)
+	private static double averageGrade(String[] grades)
 	{
-		int avgGrade=0;
-		for(int grade: grades)
+		double avgGrade=0;
+		for(String grade: grades)
 		{
-			avgGrade= avgGrade+grade;
+			avgGrade= avgGrade+ grade==null?0: Double.parseDouble(grade);
 		}
 		
-		return avgGrade/grades.length;
+		return avgGrade/(double)grades.length;
 	}
 	
 	/*
 	 * This method takes list of students and their grades and iterates through each student and prints the average grade he got
 	 */
 	
-	private static Map<String, Double> StudentsAvgGrade(Map<String, int[]> studentandGrades)
+	private static Map<String, Double> StudentsAvgGrade(Map<String, String[]> studentandGrades)
 	{
 		Map<String, Double> StudentavgGrades= new HashMap<>();
-		for(Map.Entry<String, int[]> entry: studentandGrades.entrySet())
+		for(Map.Entry<String, String[]> entry: studentandGrades.entrySet())
 		{
 			String studentId=entry.getKey();
 			Double avgGrade=averageGrade(entry.getValue());
